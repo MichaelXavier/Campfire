9b2644fe663438e92d3bc8529b29942bfb09e31e
Michael Xavier <michael@michaelxavier.net>
2011-05-01 23:54:24 -0700

    types mostly implemente

--- Web/Campfire/Types.hs
+++ Web/Campfire/Types.hs
@@ -1,13 +1,13 @@
 module Web.Campfire.Types where
 
-import Data.Time.Clock (UTCTime)
-import Data.Aeson
-import Control.Applicative
+import Control.Applicative ((<$>), (<*>))
 import Control.Monad (mzero)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TI
+import Data.Aeson
 import Data.Attoparsec (parse, maybeResult, eitherResult)
 import Data.ByteString as BS (readFile)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
+import Data.Time.Clock (UTCTime)
 
 data Room = Room { roomId               :: Integer,
                    roomName             :: T.Text,
@@ -17,6 +17,7 @@ data Room = Room { roomId               :: Integer,
                    roomOpenToGuests     :: Bool,
                    roomActiveTokenValue :: T.Text,
                    roomUpdatedAt        :: UTCTime,
+                   roomUsers            :: [User],
                    roomCreatedAt        :: UTCTime } deriving (Show)
 
 instance FromJSON Room where
@@ -36,10 +37,45 @@ newtype Rooms = Rooms { unRooms :: [Room] } deriving (Show)
 
 instance FromJSON Rooms where
   parseJSON (Object v) = Rooms <$> v .: T.pack "rooms"
-                   
+  parseJSON _          = mzero
 
---parseRooms :: BS.ByteString -> Either String [Room]
---parseRooms = eitherResult $ parse json txt
+data Message = Message { messageId        :: Integer,
+                         messageBody      :: T.Text,
+                         messageRoomId    :: Integer,
+                         messageUserId    :: T.Text,
+                         messageCreatedAt :: UTCTime,
+                         messageType      :: T.Text }
+
+instance FromJSON Message where
+  -- consider using an ADT for type
+  parseJSON (Object v) = Message <$> v .: T.pack "id"
+                                 <*> v .: T.pack "body"
+                                 <*> v .: T.pack "room-id"
+                                 <*> v .: T.pack "user-id"
+                                 <*> v .: T.pack "created-at"
+                                 <*> v .: T.pack "type"
+
+newtype Messages = Messages { unMessages :: [Room] } deriving (Show)
+
+instance FromJSON Messages where
+  parseJSON (Object v) = Messages <$> v .: T.pack "messages"
+  parseJSON _          = mzero
+
+data User = User { userId           :: Integer,
+                   userName         :: T.Text,
+                   userEmailAddress :: T.Text,
+                   userAdmin        :: Bool,
+                   userCreatedAt    :: UTCTime,
+                   userType         :: T.Text }
+
+instance FromJSON User where
+  -- consider using an ADT for type
+  parseJSON (Object v) = User <$> v .: T.pack "id"
+                              <*> v .: T.pack "name"
+                              <*> v .: T.pack "email-address"
+                              <*> v .: T.pack "admin"
+                              <*> v .: T.pack "created-at"
+                              <*> v .: T.pack "type"
 
 main :: IO ()
 main = BS.readFile "rooms.json" >>= printJSON . parsed
